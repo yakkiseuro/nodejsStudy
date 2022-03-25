8694fd6ad2b2435b20739a20b6604e4a26c9e5e3
hoon <easthoon1996@gmail.com>
2022-03-25 12:25:08 +0900

    03.17  node.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     partials = require('express-partials'),      // 라이브러리에 대한 레퍼런스 생성 
     cookieParser = require('cookie-parser'),     // 쿠키 관련 사용을 위한 레퍼런스 생성
-    session = require('express-session');        // 세션 관련 사용을 위한 레퍼런스 생성
+    session = require('express-session')        // 세션 관련 사용을 위한 레퍼런스 생성
 
 // 03.17 : routes 추가
 var routes = require('./routes');
@@ -15,11 +16,24 @@ var errorHandlers = require('./middleware/errorhandlers');
 var log = require('./middleware/log');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(log.logger);
 app.use(express.static(__dirname + '/static'));
 
 // 03.17 : cookie 사용
 app.use(cookieParser());
+
+// 03.23 : session 사용
+app.use(session({secret: 'secret'}));
+
+// 03.23 : 페이지 로드 때 마다 세션 데이터 수정 ( 책 50p )
+app.use(function (req, res, next) {
+  if (req.session.pageCount)
+    req.session.pageCount++;
+  else
+    req.session.pageCount=1;
+  next();
+});
 
 // 03.17 : routes 추가
 app.get('/', routes.index);
